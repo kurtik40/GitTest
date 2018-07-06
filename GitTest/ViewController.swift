fa8b340c3334005191f0987e1c5d03f0ed066d0d
Kurt Warson <kurtwarson@kurts-mac-mini.local>
2018-07-06 10:44:05 +0200

    add constant

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let message = "hallo git"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       print(message)
+        
     }
 
     override func didReceiveMemoryWarning() {
